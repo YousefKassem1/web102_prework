09e3ece4dd0d1996e8ba1b00a655b0edf6f919cf
Yousef Kassem <ykassem2004@gmail.com>
2025-01-18 06:36:27 -0500

    finished challenge 3

--- index.js
+++ index.js
@@ -27,31 +27,33 @@ const gamesContainer = document.getElementById("games-container");
 
 // create a function that adds all data from the games array to the page
 function addGamesToPage(games) {
-
-
-
     // loop over each item in the data
-
-
-        // create a new div element, which will become the game card
-
-
-        // add the class game-card to the list
-
-
-        // set the inner HTML using a template literal to display some info 
-        // about each game
-        // TIP: if your images are not displaying, make sure there is space
-        // between the end of the src attribute and the end of the tag ("/>")
-
-
-        // append the game to the games-container
-
+    for (let i = 0;i<games.length;i++)
+    {
+       // create a new div element, which will become the game card
+       const game = games[i];
+       // add the class game-card to the list
+       const gameCard = document.createElement("div");
+       gameCard.classList.add("game-card");
+       // set the inner HTML using a template literal to display some info 
+       // about each game
+       // TIP: if your images are not displaying, make sure there is space
+       // between the end of the src attribute and the end of the tag ("/>")
+       gameCard.innerHTML = `
+           <img class = "game-img" src="${game.img}" alt="${game.name}" />
+           <h2>${game.name}</h2>
+           <p>Goal: $${game.goal.toLocaleString()}</p>
+           <p>Pledged: $${game.pledged.toLocaleString()}</p>
+           <p>Backers: ${game.backers.toLocaleString()}</p>
+       `;
+       // append the game to the games-container
+       gamesContainer.appendChild(gameCard);
+     }
 }
 
 // call the function we just defined using the correct variable
 // later, we'll call this function using a different list of games
-
+ addGamesToPage(GAMES_JSON);
 
 /*************************************************************************************
  * Challenge 4: Create the summary statistics at the top of the page displaying the
