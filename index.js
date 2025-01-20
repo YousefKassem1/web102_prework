da28c00e416925858ab5b012dd90293bc18e7717
Yousef Kassem <ykassem2004@gmail.com>
2025-01-19 21:45:10 -0500

    finished challenge 5 started challenge 6

--- index.js
+++ index.js
@@ -93,9 +93,9 @@ function filterUnfundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have not yet met their goal
-
-
+    const myGames = GAMES_JSON.filter(game => game.pledged < game.goal);
     // use the function we previously created to add the unfunded games to the DOM
+    addGamesToPage(myGames);
 
 }
 
@@ -104,9 +104,11 @@ function filterFundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have met or exceeded their goal
-
+    const myGames = GAMES_JSON.filter(game => game.pledged >= game.goal);
+    console.log(myGames);
 
     // use the function we previously created to add unfunded games to the DOM
+    addGamesToPage(myGames);
 
 }
 
@@ -115,6 +117,7 @@ function showAllGames() {
     deleteChildElements(gamesContainer);
 
     // add all games from the JSON data to the DOM
+    addGamesToPage(GAMES_JSON);
 
 }
 
@@ -124,6 +127,9 @@ const fundedBtn = document.getElementById("funded-btn");
 const allBtn = document.getElementById("all-btn");
 
 // add event listeners with the correct functions to each button
+unfundedBtn.addEventListener("click", filterUnfundedOnly);
+fundedBtn.addEventListener("click", filterFundedOnly);
+allBtn.addEventListener("click", showAllGames);
 
 
 /*************************************************************************************
@@ -135,12 +141,16 @@ const allBtn = document.getElementById("all-btn");
 const descriptionContainer = document.getElementById("description-container");
 
 // use filter or reduce to count the number of unfunded games
-
+const unfundedGames = GAMES_JSON.filter(game => game.pledged < game.goal).length;
 
 // create a string that explains the number of unfunded games using the ternary operator
-
+const unfundedString = unfundedGames === 0 ? "all" : unfundedGames;
 
 // create a new DOM element containing the template string and append it to the description container
+const description = document.createElement("p");
+description.innerHTML = `We currently have ${GAMES_JSON.length} games on our site.
+${unfundedString} of them are still looking for backers.`;
+descriptionContainer.appendChild(description);
 
 /************************************************************************************
  * Challenge 7: Select & display the top 2 games
